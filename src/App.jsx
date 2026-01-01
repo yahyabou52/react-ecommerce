a335b9d6752e3cb769d1ac2b30e8b70c90c25c82
yahyabouhafs <yahyabou52@gmail.com>
2026-01-01 01:15:59 +0100

    simple commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,31 @@ import ProductCard from "./components/ProductCard";
 import { products } from "./data/products";
 function App() {
   const [search, setSearch] = useState("");
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase())
-  );
+  const [category, setCategory] = useState("All");
+  const subCategories = [
+  ...new Set(
+    products
+      .filter(
+        (p) => category === "All" || p.category === category
+      )
+      .map((p) => p.subCategory)
+  ),
+];
+  const [subCategory, setSubCategory] = useState("All");
+  const filteredProducts = products.filter((product) => {
+  const matchSearch = product.name
+    .toLowerCase()
+    .includes(search.toLowerCase());
+
+  const matchCategory =
+    category === "All" || product.category === category;
+
+  const matchSubCategory =
+    subCategory === "All" || product.subCategory === subCategory;
+
+  return matchSearch && matchCategory && matchSubCategory;
+});
+
   return (
     <div>
       <h1>My First React E-commerce</h1>
@@ -13,7 +35,40 @@ function App() {
         placeholder="Search products..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-      />      
+      />
+    <div className="filters">
+      <button onClick={() => {
+        setCategory("All");
+        setSubCategory("All");
+      }}>
+        All
+      </button>
+
+      <button onClick={() => {
+        setCategory("Clothing");
+        setSubCategory("All");
+      }}>
+        Clothing
+      </button>
+
+      <button onClick={() => {
+        setCategory("Accessories");
+        setSubCategory("All");
+      }}>
+        Accessories
+      </button>
+    </div>
+
+    {category !== "All" && (
+      <div className="filters">
+        {subCategories.map((sub) => (
+          <button key={sub} onClick={() => setSubCategory(sub)}>
+            {sub}
+          </button>
+        ))}
+      </div>
+    )}
+
       {filteredProducts.map((product) => (
         <ProductCard
           key={product.id} 
